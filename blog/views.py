d951c8ce8a6396f1dee09f36733bf36d1921e563
Nadia <redinskikh@gmail.com>
2016-02-01 16:48:09 +0300

    Added a local email server

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm, CommentForm, ContactForm
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.template import Context
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -79,4 +83,35 @@ def comment_remove(request, pk):
     return redirect('blog.views.post_detail', pk=post_pk)
 def contact(request):
     form_class = ContactForm
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+            , '')
+            contact_email = request.POST.get(
+                'contact_email'
+            , '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the 
+            # contact information
+            template = get_template('blog/contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" +'',
+                ['youremail@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('contact')
     return render(request,'blog/contact.html', {'form': form_class,})
